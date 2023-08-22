4025332bd42eca57ccfca1bcc6554b522bdf20d9
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2023-08-22 10:37:38 -0300

    test refactor out ddl setup

--- test/rinha/main_test.clj
+++ test/rinha/main_test.clj
@@ -11,53 +11,58 @@
 ;; start a psql:
 ;; docker run -e POSTGRES_PASSWORD=postgres --rm -p 5432:5432 postgres:alpine
 
-(deftest hello
+(defn setup-database
+  []
   (let [{::rinha/keys [conn]
          ::http/keys  [service-fn]} (-> {::rinha/jdbc-url "jdbc:postgres://localhost:5432/postgres?user=postgres&password=postgres"}
-                                      rinha/default-interceptors
-                                      http/dev-interceptors
-                                      http/create-servlet)]
+                                        rinha/default-interceptors
+                                        http/dev-interceptors
+                                        http/create-servlet)]
     (jdbc/execute! conn ["DROP TABLE IF EXISTS stack"])
     (jdbc/execute! conn ["DROP TABLE IF EXISTS pessoa"])
     (jdbc/execute! conn [(slurp (io/resource "schema.sql"))])
+    service-fn))
+
+(deftest hello
+  (let [service-fn (setup-database)]
     (is (= 0
-          (-> service-fn
-            (response-for :get "/contagem-pessoas")
-            :body
-            json/parse-string
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :get "/contagem-pessoas")
+               :body
+               json/parse-string
+               #_(doto clojure.pprint/pprint))))
     (is (= "/pessoas/josé"
-          (-> service-fn
-            (response-for :post "/pessoas"
-              :headers {"Content-Type" "application/json"}
-              :body (json/generate-string {:apelido    "josé"
-                                           :nome       "José Roberto"
-                                           :nascimento "2000-10-01"
-                                           :stack      ["C#" "Node" "Oracle"]}))
-            :headers
-            (get "Location")
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :post "/pessoas"
+                             :headers {"Content-Type" "application/json"}
+                             :body (json/generate-string {:apelido    "josé"
+                                                          :nome       "José Roberto"
+                                                          :nascimento "2000-10-01"
+                                                          :stack      ["C#" "Node" "Oracle"]}))
+               :headers
+               (get "Location")
+               #_(doto clojure.pprint/pprint))))
     (is (= "/pessoas/ana"
-          (-> service-fn
-            (response-for :post "/pessoas"
-              :headers {"Content-Type" "application/json"}
-              :body (json/generate-string {:apelido    "ana"
-                                           :nascimento "1985-09-23"
-                                           :nome       "Ana Barbosa"
-                                           :stack      ["Node" "Postgres"]}))
-            :headers
-            (get "Location")
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :post "/pessoas"
+                             :headers {"Content-Type" "application/json"}
+                             :body (json/generate-string {:apelido    "ana"
+                                                          :nascimento "1985-09-23"
+                                                          :nome       "Ana Barbosa"
+                                                          :stack      ["Node" "Postgres"]}))
+               :headers
+               (get "Location")
+               #_(doto clojure.pprint/pprint))))
 
     (is (= {:apelido    "josé"
             :nascimento "2000-09-30"
             :nome       "José Roberto"
             :stack      ["C#" "Node" "Oracle"]}
-          (-> service-fn
-            (response-for :get "/pessoas/josé")
-            :body
-            (json/parse-string true)
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :get "/pessoas/josé")
+               :body
+               (json/parse-string true)
+               #_(doto clojure.pprint/pprint))))
     (is (= [{:apelido    "josé",
              :nome       "José Roberto",
              :nascimento "2000-09-30",
@@ -66,34 +71,34 @@
              :nome       "Ana Barbosa",
              :nascimento "1985-09-22",
              :stack      ["Node" "Postgres"]}]
-          (-> service-fn
-            (response-for :get "/pessoas?t=node")
-            :body
-            (json/parse-string true)
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :get "/pessoas?t=node")
+               :body
+               (json/parse-string true)
+               #_(doto clojure.pprint/pprint))))
     (is (= [{:apelido    "josé",
              :nome       "José Roberto",
              :nascimento "2000-09-30",
              :stack      ["C#" "Node" "Oracle"]}]
-          (-> service-fn
-            (response-for :get "/pessoas?t=berto")
-            :body
-            (json/parse-string true)
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :get "/pessoas?t=berto")
+               :body
+               (json/parse-string true)
+               #_(doto clojure.pprint/pprint))))
     (is (= []
-          (-> service-fn
-            (response-for :get "/pessoas?t=Python")
-            :body
-            (json/parse-string true)
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :get "/pessoas?t=Python")
+               :body
+               (json/parse-string true)
+               #_(doto clojure.pprint/pprint))))
     (is (= 400
-          (-> service-fn
-            (response-for :get "/pessoas")
-            :status
-            #_(doto clojure.pprint/pprint))))
+           (-> service-fn
+               (response-for :get "/pessoas")
+               :status
+               #_(doto clojure.pprint/pprint))))
     (is (= 2
-          (-> service-fn
-            (response-for :get "/contagem-pessoas")
-            :body
-            json/parse-string
-            #_(doto clojure.pprint/pprint))))))
+           (-> service-fn
+               (response-for :get "/contagem-pessoas")
+               :body
+               json/parse-string
+               #_(doto clojure.pprint/pprint))))))
